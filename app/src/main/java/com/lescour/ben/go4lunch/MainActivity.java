f7dac9bafe17b36fe481d7631bf493ec39135791
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-03-14 13:40:41 +0100

    Add a menu drawer

--- app/src/main/java/com/lescour/ben/go4lunch/MainActivity.java
+++ app/src/main/java/com/lescour/ben/go4lunch/MainActivity.java
@@ -9,12 +9,23 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Arrays;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.activity_main_toolbar) Toolbar toolbar;
+    @BindView(R.id.activity_main_drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.activity_main_nav_view) NavigationView navigationView;
 
     private TextView mTextMessage;
     private static final int RC_SIGN_IN = 123;
@@ -23,12 +34,21 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        this.configureToolbar();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         this.checkUserIsLogged();
+
+        this.configureDrawerLayout();
+    }
+
+    private void configureToolbar() {
+        setSupportActionBar(toolbar);
     }
 
     //BOTTOM TOOLBAR\\
@@ -103,6 +123,50 @@ public class MainActivity extends BaseActivity {
                         .setLogo(R.drawable.go4lunch_ic_sign)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    //MAIN MENU\\
+    private void configureDrawerLayout(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureNavigationView(){
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /**
+     * When an item is selected. Display the associate fragment in the ViewPager.
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.activity_main_drawer_your_lunch:
+                break;
+            case R.id.activity_main_drawer_settings:
+                break;
+            case R.id.activity_main_drawer_logout:
+                break;
+            default:
+                break;
+        }
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * Close the NavigationDrawer with the button back
+     */
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
